34e7328e6201ec146734e5d325f015670be80f5c
Ilham Ellya Khahar <ilham.ellya@nawatech.co>
2025-07-09 15:30:22 +0700

    updated

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-research-terraform"
+    resource_group_name  = "rg-test"
     storage_account_name = "research9tfstate"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"

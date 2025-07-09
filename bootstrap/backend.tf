f49d19f53d1051992604c15c86fa45db07916f91
Ilham Ellya Khahar <ilham.ellya@nawatech.co>
2025-07-09 14:27:48 +0700

    deleted bootstrap

--- bootstrap/backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "rg-research-terraform"
-#     storage_account_name = "research9tfstate"
-#     container_name       = "tfstate"
-#     key                  = "bootstrap.terraform.tfstate"
-#   }
-# }
